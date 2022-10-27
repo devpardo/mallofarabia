2bf9be56f025d04abbb1d7942f6348eeba2f2f9e
Bryan Pardo <devpardo@gmail.com>
2022-10-27 17:05:02 +0800

    Complete mobile menu.

--- components/base/MobileMenu.vue
+++ components/base/MobileMenu.vue
@@ -22,8 +22,52 @@
             </v-col>
         </v-row>
 
-        <div class="mobile-menu white slide-y-transition" v-if="menuShown">
-            adsfadsfadsf
+        <div class="mobile-menu white" v-if="menuShown">
+            <div class="pa-5">
+                <v-list class="mb-10">
+                    <div v-for="(item, index) in mainLinks" :key="index">
+                        <v-list-group class="mb-2 rounded-lg pill-menu" v-if="item.children.length">
+                            <template v-slot:activator>
+                                <v-list-item-content>
+                                    <v-list-item-title v-text="item.name"></v-list-item-title>
+                                </v-list-item-content>
+                            </template>
+
+                            <v-list-item
+                                v-for="child in item.children"
+                                :key="child.name"
+                                @click="menuActionClick(item)"
+                            >
+                                <v-list-item-content>
+                                    <v-list-item-title v-text="child.name"></v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list-group>
+
+                        <v-list-item @click="menuActionClick(item)" class="mb-2 rounded-lg pill-menu" v-else>
+                            <v-list-item-content>
+                                <v-list-item-title v-text="item.name"></v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </div>
+                </v-list>
+
+                <v-divider></v-divider>
+
+                <v-list class="mt-10 mb-10">
+                    <v-list-item class="item-link mb-2" v-for="(item, index) in links" :key="index">
+                        <v-list-item-content>
+                            <v-list-item-title class="item-link-title">
+                                <v-icon class="mr-1 icon">{{ item.icon }}</v-icon> {{ item.name }}
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list>
+            </div>
+
+            <div class="pa-2 grey lighten-3 text-center">
+                Developed by FNDev
+            </div>
         </div>
    </div>
 </template>
@@ -55,6 +99,11 @@ export default {
         window.removeEventListener('resize', () => {
             console.log('removed');
         })
+    },
+    methods: {
+        menuActionClick(param) {
+            console.log(param)
+        }
     }
 }
 </script>
@@ -64,22 +113,6 @@ export default {
     width: 150px;
 }
 
-.toplink-item {
-    padding: 20px 0;
-    font-size: 14px;
-}
-
-.icon {
-    font-size: 15px;
-    color: $charcoal-grey;
-}
-
-.main-link {
-    .link-item {
-        font-size: 16px;
-    }
-}
-
 .mobile-menu {
     width: 100%;
     position: absolute;
@@ -87,6 +120,38 @@ export default {
     top: 70px;
     height: 100%;
     animation: fadeIn .5s;
+
+    .pill-menu {
+        border: 1px solid $charcoal-grey;
+    }
+
+    .v-list-item.v-list-item--active::before {
+        display: none !important;
+        background: red;
+    }
+
+    .v-list-item--active:focus::before {
+        opacity: 1 !important;
+    }
+
+    .theme--light.v-list-item--active:focus::before {
+        opacity: 0 !important;
+    }
+
+    .theme--light.v-list-item--active:hover::before, .theme--light.v-list-item--active::before {
+        display: none !important;
+    }
+
+    .item-link {
+
+        .item-link-title {
+            font-size: 1em;
+        }
+
+        .icon {
+            font-size: 1.2em;
+        }
+    }
 }
 
 @keyframes fadeIn {
