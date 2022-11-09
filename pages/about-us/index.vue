f9fb409753ebf8e5677b1afa1262c13c80566a8a
Bryan Pardo <bryan.pardo@nodifi.com.au>
2022-11-09 12:13:08 +0800

    About us page

---
+++ pages/about-us/index.vue
@@ -0,0 +1,76 @@
+<template>
+    <div>
+        <PageHeader :pageBg="pageBg" :title="contents.page_label" :subtitle="contents.page_description"/>
+
+        <v-row>
+            <v-col>
+                <v-container>
+                    <v-row>
+                        <v-col cols="6" v-for="(content, index) in contents.data" :key="index" class="my-10">
+                            <div class="px-10">
+                                <v-img height="200" :src="content.image" ></v-img>
+                                <h4 class="text-uppercase mt-10">{{ content.name }}</h4>
+                                <h1 class="mt-2 mb-10">{{ content.title }}</h1>
+
+                                <p>{{ content.content }}</p>
+                            </div>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-col>
+        </v-row>
+        
+
+        <Loading :loading="loading"/>
+    </div>
+</template>
+
+<script>
+
+import PageHeader from '@/components/PageHeader'
+import Loading from '@/components/Loading'
+
+export default {
+    components: {
+        Loading,
+        PageHeader
+    },
+    data() {
+        return {
+            loading: false,
+            contents: {
+                page_description:'',
+                page_image: '',
+                page_keywords:'',
+                page_label: '',
+                data: null
+            }
+        }
+    },
+    computed: {
+        pageBg() {
+            return { 'background-image': `url(${this.contents?.page_image})` }
+        }
+    },
+    methods: {
+        async pull() {
+            try {
+                this.loading = true;
+                let res =  await this.$api.get('/about_us');
+                this.contents = {...res.data}
+                this.loading = false;
+            } catch (error) {
+                console.log(error);
+                this.loading = false;
+            }
+        }
+    },
+    mounted() {
+        this.pull();
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
