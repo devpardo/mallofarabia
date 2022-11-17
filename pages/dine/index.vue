57677dcc0cd4a8ab7148c2c2eed902499f33b7d8
Bryan Pardo <bryan.pardo@nodifi.com.au>
2022-11-17 09:18:59 +0800

    Update dine, Update entertain pages

--- pages/dine/index.vue
+++ pages/dine/index.vue
@@ -7,9 +7,131 @@
                 <v-container>
                     <!-- {{ contents.data }} -->
 
-                    <div>
-                        <pre>  {{ contents.data }}</pre>
-                    </div>
+                    <v-row>
+                        <v-col v-if="!contents.data.length">
+                            <h2>No Data Available</h2>
+                        </v-col>
+                        <v-col v-else>
+                            <v-row no-gutters>
+                                <v-col cols="12" md="3" :order="!vb.smAndDown ? 'first': 'last'">
+                                    <v-list>
+                                        <h2 class="mb-3">Categories</h2> 
+
+                                        <v-list-item @click="test(-1)">
+                                            <v-list-item-content>
+                                                <v-list-item-title>All</v-list-item-title>
+                                            </v-list-item-content>
+                                        </v-list-item>
+                                        <v-divider></v-divider>
+
+                                        <div v-for="(item, i) in categories" :key="i">
+                                            <v-list-item v-if="!item.sub_categories.length" @click="test(i)">
+                                                <v-list-item-content>
+                                                    <v-list-item-title>{{ item.name }} ({{ item.tenants_count }})</v-list-item-title>
+                                                </v-list-item-content>
+                                            </v-list-item>
+                                            
+
+                                            <v-list-group v-else
+                                                :value="true">
+
+                                                <template v-slot:activator>
+                                                    <v-list-item-title>{{ item.name }} ({{ item.tenants_count }})</v-list-item-title>
+                                                </template>
+
+                                                <v-list-item  v-for="(sub, idx) in item.sub_categories" :key="idx">
+                                                    {{ sub.name }} ({{ sub.tenants_count }})
+                                                </v-list-item>
+
+                                            </v-list-group>
+
+                                            <v-divider></v-divider>
+                                        </div>
+                                    </v-list>
+                                </v-col>
+                                <v-col cols="12" md="9" class="pa-5">
+                                    <v-card class="mb-3 pb-3 px-3 pt-5" v-for="(content, index) in contents.data" :key="index" >
+                                        <v-card-title class="d-flex justify-space-between align-center" dir="auto">
+                                            <div>
+                                                 <v-img v-if="$i18n.locale !== 'ar'" :src="content.logo_en" contain width="100"></v-img>
+                                                 <v-img v-else :src="content.logo_ar" contain width="100"></v-img>
+                                            </div>
+
+                                            <div style="width: 80%">
+                                                <h3>{{ content.name }}</h3>
+                                                <p class="caption">{{ content.description }}</p>
+                                            </div>
+                                        </v-card-title>
+                                        <v-card-text>
+                                            <div>
+                                                <v-carousel v-if="content.images.length" hide-delimiter-background hide-delimiters height="200">
+                                                    <v-carousel-item
+                                                        v-for="(item,i) in content.images"
+                                                        :key="i"
+                                                        :src="item.path"
+                                                        reverse-transition="fade-transition"
+                                                        transition="fade-transition"
+                                                    ></v-carousel-item>
+                                                </v-carousel>
+                                            </div>
+                                        </v-card-text>
+                                        <v-card-text>
+                                            <v-row>
+                                                <v-col cols="12" md="8">
+                                                    <v-row>
+                                                        <v-col cols="6" md="4">
+                                                            <div class="text-center">
+                                                                <h3><v-icon small class="mr-3" color="primary">mdi-clock</v-icon>Working Hours</h3>
+                                                                <p>{{ content.working_hours }}</p>
+                                                            </div>
+                                                        </v-col>
+                                                        <v-col cols="6" md="4">
+                                                            <div class="text-center">
+                                                                <h3><v-icon small class="mr-3" color="primary">mdi-phone</v-icon> Store Numbers</h3>
+                                                                <ul class="list-unstyled">
+                                                                    <li v-for="(phone, i) in content.phones" :key="i">
+                                                                        {{ phone }}
+                                                                    </li>
+                                                                </ul>
+                                                            </div>
+                                                        </v-col>
+                                                        <v-col cols="6" md="4">
+                                                            <div class="text-center">
+                                                                <h3><v-icon small class="mr-3" color="primary">mdi-map-marker</v-icon>Nearest Gates</h3>
+                                                                <ul class="list-unstyled">
+                                                                    <li v-for="(gate, i) in content.gates" :key="i">
+                                                                        {{ gate.number }}
+                                                                    </li>
+                                                                </ul>
+                                                            </div>
+                                                        </v-col>
+                                                        <v-col cols="12">
+                                                            <!-- <v-btn
+                                                                v-for="(item, i) in content.social_links" :key="i"
+                                                                depressed
+                                                                text
+                                                                fab
+                                                                dark
+                                                                small
+                                                                color="grey"
+                                                                class="mr-1"
+                                                                @click="goLink(item.link_value)"
+                                                            >
+                                                                <v-icon>
+                                                                    {{ sm_links[item.link_type]['icon'] }}
+                                                                </v-icon>
+                                                            </v-btn> -->
+                                                            
+                                                        </v-col>
+                                                    </v-row>
+                                                </v-col>
+                                            </v-row>
+                                        </v-card-text>
+                                    </v-card>
+                                </v-col>
+                            </v-row>
+                        </v-col>
+                    </v-row>
                 </v-container>
             </v-col>
         </v-row>
@@ -44,6 +166,9 @@ export default {
     computed: {
         pageBg() {
             return { 'background-image': `url(${this.contents?.page_image})` }
+        },
+        vb() {
+            return this.$vuetify.breakpoint
         }
     },
     methods: {
--- pages/entertain/index.vue
+++ pages/entertain/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <PageHeader :pageBg="pageBg" :title="contents.page_label" :subtitle="contents.page_description"/>
+        <PageHeader :pageBg="pageBg" :title="contents.page_name" :subtitle="contents.page_description"/>
 
         <v-row>
             <v-col>
@@ -9,11 +9,91 @@
                         <v-col v-if="!contents.data.length">
                             <h2>No Data Available</h2>
                         </v-col>
-                        <v-col v-else><pre>{{ contents.data }}</pre></v-col>
+                        <v-col v-else>
+                            <v-row>
+                                <v-col cols="12" v-for="(content, i) in contents.data" :key="i">
+                                    <v-img :src="content.cover" class="rounded-xl d-flex align-center justify-center" height="300" @click="seeMore(content)">
+                                        <div class="text-center white--text mx-auto" style="width: 80%">
+                                            <h1>{{ content.name }}</h1>
+                                            <p class="body-1">{{ content.description }}</p>
+
+                                            <v-btn color="secondary" small @click="seeMore(content)">See more</v-btn>
+                                        </div>
+                                    </v-img>
+                                </v-col>
+                            </v-row>
+                        </v-col>
                     </v-row>
                 </v-container>
             </v-col>
         </v-row>
+
+        <v-dialog
+            persistent
+            v-model="dialog"
+            min-width="300"
+            max-width="1000"
+            >
+            <v-card v-if="selected != null" class="pa-5 rounded-xl">
+                <v-card-title class="mb-10">
+                    <v-row>
+                        <v-col cols="12" md="2">
+                            <v-img :src="selected.logo_en" width="200" contain></v-img>
+                        </v-col>
+                        <v-col cols="12" md="5">
+                                <h3>{{ selected.name }}</h3>
+                                <p class="caption">{{ selected.description }}</p>
+                        </v-col>
+                        <v-col cols="12" md="5">
+                            <h3>Social Links</h3>
+                        </v-col>
+                    </v-row>
+                </v-card-title>
+
+                <v-card-text>
+                    <v-row>
+                        <v-col cols="6" md="4">
+                            <div class="text-center">
+                                <h3><v-icon small class="mr-3" color="primary">mdi-clock</v-icon>Working Hours</h3>
+                                <p>{{ selected.working_hours }}</p>
+                            </div>
+                        </v-col>
+                        <v-col cols="6" md="4">
+                            <div class="text-center">
+                                <h3><v-icon small class="mr-3" color="primary">mdi-phone</v-icon> Store Numbers</h3>
+                                <ul class="list-unstyled">
+                                    <li v-for="(phone, i) in selected.phones" :key="i">
+                                        {{ phone }}
+                                    </li>
+                                </ul>
+                            </div>
+                        </v-col>
+                        <v-col cols="6" md="4">
+                            <div class="text-center">
+                                <h3><v-icon small class="mr-3" color="primary">mdi-map-marker</v-icon>Nearest Gates</h3>
+                                <ul class="list-unstyled">
+                                    <li v-for="(gate, i) in selected.gates" :key="i">
+                                        {{ gate.number }}
+                                    </li>
+                                </ul>
+                            </div>
+                        </v-col>
+                    </v-row>
+                </v-card-text>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+
+                    <v-btn
+                        color="primary"
+                        text
+                        @click="dialog = false"
+                    >
+                        Close
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
         
 
         <Loading :loading="loading"/>
@@ -33,11 +113,13 @@ export default {
     data() {
         return {
             loading: false,
+            selected: null,
+            dialog: false,
             contents: {
                 page_description:'',
                 page_image: '',
                 page_keywords:'',
-                page_label: '',
+                page_name: '',
                 data: {}
             }
         }
@@ -48,11 +130,20 @@ export default {
         }
     },
     methods: {
+        seeMore(item) {
+            this.dialog = true;
+            this.selected = {...item}
+        },
+        close() {
+            this.dialog = false;
+        },
         async pull() {
             try {
                 this.loading = true;
-                let res =  await this.$api.get('/mall_events');
+                let res =  await this.$api.get('/entertain');
                 this.contents = {...res.data}
+
+                console.log(res);
                 this.loading = false;
             } catch (error) {
                 console.log(error);
